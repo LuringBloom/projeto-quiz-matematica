50a4fae6c32f589591e3e3d9cb5c9a4d7b3baf6d
LuringBloom <141141612+LuringBloom@users.noreply.github.com>
2024-09-03 01:03:05 +0900

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ const quiz = [
         subject: "materia 1",
         exercises: [
             {
-                question: "Quanto e 1 + 1?",
+                question: "Quanto é 1 + 1?",
                 answers: [
                     {option: "3", correct: false},
                     {option: "2", correct: true},
@@ -12,21 +12,21 @@ const quiz = [
                 ]
             },
             {
-                question: "Quanto e 20 - 14?",
+                question: "Quanto é 20 + 14?",
                 answers: [
-                    {option: "6", correct: true},
-                    {option: "5", correct: false},
-                    {option: "7", correct: false},
-                    {option: "8", correct: false}
+                    {option: "34", correct: true},
+                    {option: "35", correct: false},
+                    {option: "30", correct: false},
+                    {option: "32", correct: false}
                 ]
             },
             {
-                question: "Quanto e 9 * 21?",
+                question: "Quanto é 13 + 57?",
                 answers: [
-                    {option: "180", correct: false},
-                    {option: "183", correct: false},
-                    {option: "189", correct: true},
-                    {option: "185", correct: false}
+                    {option: "68", correct: false},
+                    {option: "69", correct: false},
+                    {option: "70", correct: true},
+                    {option: "72", correct: false}
                 ]
             },
         ]
@@ -35,16 +35,16 @@ const quiz = [
         subject: "materia 2",
         exercises: [
             {
-                question: "Quanto e 3 + 9?",
+                question: "Quanto é 92 - 9?",
                 answers: [
-                    {option: "13", correct: false},
-                    {option: "12", correct: true},
-                    {option: "15", correct: false},
-                    {option: "14", correct: false}
+                    {option: "80", correct: false},
+                    {option: "81", correct: true},
+                    {option: "82", correct: false},
+                    {option: "79", correct: false}
                 ]
             },
             {
-                question: "Quanto e 90 - 57?",
+                question: "Quanto é 90 - 57?",
                 answers: [
                     {option: "33", correct: true},
                     {option: "35", correct: false},
@@ -53,12 +53,12 @@ const quiz = [
                 ]
             },
             {
-                question: "Quanto e 20 * 20?",
+                question: "Quanto é 30 - 17?",
                 answers: [
-                    {option: "402", correct: false},
-                    {option: "40", correct: false},
-                    {option: "400", correct: true},
-                    {option: "390", correct: false}
+                    {option: "12", correct: false},
+                    {option: "14", correct: false},
+                    {option: "13", correct: true},
+                    {option: "18", correct: false}
                 ]
             },
         ]
@@ -67,29 +67,29 @@ const quiz = [
         subject: "materia 3",
         exercises: [
             {
-                question: "Quanto e 1 + 1?",
+                question: "Quanto é 19 * 3?",
                 answers: [
-                    {option: "3", correct: false},
-                    {option: "2", correct: true},
-                    {option: "5", correct: false},
-                    {option: "4", correct: false}
+                    {option: "56", correct: false},
+                    {option: "57", correct: true},
+                    {option: "50", correct: false},
+                    {option: "60", correct: false}
                 ]
             },
             {
-                question: "Quanto e 20 - 14?",
+                question: "Quanto é 15 * 6?",
                 answers: [
-                    {option: "6", correct: true},
-                    {option: "5", correct: false},
-                    {option: "7", correct: false},
-                    {option: "8", correct: false}
+                    {option: "80", correct: false},
+                    {option: "96", correct: false},
+                    {option: "87", correct: false},
+                    {option: "90", correct: true}
                 ]
             },
             {
-                question: "Quanto e 9 * 21?",
+                question: "Quanto é 9 * 21?",
                 answers: [
                     {option: "180", correct: false},
                     {option: "183", correct: false},
-                    {option: "189", correct: true},
+                    {option: "188", correct: true},
                     {option: "185", correct: false}
                 ]
             },
@@ -99,30 +99,30 @@ const quiz = [
         subject: "materia 4",
         exercises: [
             {
-                question: "Quanto e 1 + 1?",
+                question: "Quanto é 20 / 4?",
                 answers: [
                     {option: "3", correct: false},
-                    {option: "2", correct: true},
-                    {option: "5", correct: false},
+                    {option: "2", correct: false},
+                    {option: "5", correct: true},
                     {option: "4", correct: false}
                 ]
             },
             {
-                question: "Quanto e 20 - 14?",
+                question: "Quanto é 32 / 8?",
                 answers: [
-                    {option: "6", correct: true},
+                    {option: "4", correct: true},
+                    {option: "3", correct: false},
                     {option: "5", correct: false},
-                    {option: "7", correct: false},
-                    {option: "8", correct: false}
+                    {option: "6", correct: false}
                 ]
             },
             {
-                question: "Quanto e 9 * 21?",
+                question: "Quanto é 420 / 21?",
                 answers: [
-                    {option: "180", correct: false},
-                    {option: "183", correct: false},
-                    {option: "189", correct: true},
-                    {option: "185", correct: false}
+                    {option: "19", correct: false},
+                    {option: "18", correct: false},
+                    {option: "20", correct: true},
+                    {option: "30", correct: false}
                 ]
             },
         ]
